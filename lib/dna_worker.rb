2452e5daf1dc6348085ab4490ac078e6b7d52293
Derek Owusu-Frimpong <deepsky_5@live.com>
2021-11-29 03:53:00 +0000

    refactor code into gem

---
+++ lib/dna_worker.rb
@@ -0,0 +1,36 @@
+require 'genetic_constants'
+
+module DnaWorker
+    def self.print_complementary_base_pairing(sequence)
+        sequence = sequence.map(&:to_sym)
+
+        mRNA_sequence = sequence.map { |base| GeneticConstants.nitrogenous_base_pairs[base] }
+
+        mRNA_sequence
+    end
+
+    
+    def self.initiate_translation(codon_collection)
+      puts "intiating translation of mRNA sequence"
+      puts ""
+      puts "resulting amino acid sequence..."
+      puts ""
+  
+      codon_collection.each do |codon|
+        if GeneticConstants.genetic_code[codon.join("")].nil?
+          next
+        elsif GeneticConstants.genetic_code[codon.join("")] == "STOP"
+          print "#{GeneticConstants.genetic_code[codon.join("")]}"
+          puts ""
+          puts "=>translation of mRNA sequence terminated.<="
+          break
+        else
+          print "#{GeneticConstants.genetic_code[codon.join("")]}---"
+        end
+      end
+      print "STOP"
+      puts ""
+    end
+  end
+  
+  
